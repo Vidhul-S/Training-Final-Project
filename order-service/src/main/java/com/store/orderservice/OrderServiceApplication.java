82bca66cf5dd9e9eabf08e1a11a34961c881a840
Vidhul S <vidhuls1003@gmail.com>
2025-03-11 22:14:59 +0530

    commit

--- order-service/src/main/java/com/store/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/store/orderservice/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.store.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.store.orderservice.config")
 @SpringBootApplication
 public class OrderServiceApplication {
 
